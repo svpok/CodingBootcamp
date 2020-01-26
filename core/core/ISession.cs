79c11a6094642866d365a80fb7375c6a33cbc033
yehya1 <abohdaedyehya5@gmail.com>
2020-01-26 10:36:00 +0200

    starting write ISession interface and  SessionInfo

---
+++ core/core/ISession.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SupportTroubleshootingTool.Core.Contracts
+{
+
+    interface ISession
+    {
+        SessionInfo CurrentSession();
+        void StartSession(SessionInfo session);
+        void StopSession(SessionInfo session);
+        void CollectData(SessionInfo session);
+
+    }
+    class SessionInfo : ISession
+    {
+
+        SessionInfo ISession.CurrentSession()
+        {
+            SessionInfo a =new SessionInfo();
+            return a;
+        }
+        void ISession.StartSession(SessionInfo session)
+        {
+
+        }
+        void ISession.StopSession(SessionInfo session)
+        {
+
+        }
+        void ISession.CollectData(SessionInfo session)
+        {
+
+        }
+
+
+    }
+
+
+}
