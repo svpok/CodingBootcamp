5fa446289c0fc22c0cc933455e87476dd12ba46f
unknown <jake@google.com>
2020-01-29 14:13:02 +0200

    add 2 forms with names in Ui

--- SupportTroubleshootingTool/WindowsFormsApp1/NewSessionFormUi.Designer.cs
+++ SupportTroubleshootingTool/WindowsFormsApp1/NewSessionFormUi.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class NewSessionFormUi
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-
+            this.Text = "NewSessionFormUi";
         }
 
         #endregion
     }
 }
-
