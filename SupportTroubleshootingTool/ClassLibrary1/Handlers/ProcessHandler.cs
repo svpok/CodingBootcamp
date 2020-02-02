4e60fde98544700b22e9ecbd071fd41d51eeb370
aakef <aakef1987@gmail.com>
2020-02-02 15:40:46 +0200

    Add Handlers -> Process and Packaging

---
+++ SupportTroubleshootingTool/ClassLibrary1/Handlers/ProcessHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportTroubleshootingTool.Core.Handlers
+{
+    class ProcessHandler  
+    {
+
+        public static void RestartService(string serviceName, int timeoutMilliseconds)  // Will handle services that running in server 
+        {
+            ServiceController service = new ServiceController(serviceName);
+            try
+            {
+                int millisec1 = Environment.TickCount;
+                TimeSpan timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+
+                // count the rest of the timeout
+                int millisec2 = Environment.TickCount;
+                timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds - (millisec2 - millisec1));
+
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+            }
+            catch (Exception e)
+            {
+                Utilities.Logger.WriteError(e);
+            }
+        }
+    }
+}
