f06ad214d9ec4835336e24c7081cd2f1a973faeb
aakef <aakef1987@gmail.com>
2020-02-22 03:06:43 +0200

    Finished FilesHandlers

--- SupportTroubleshootingTool/Core/Handlers/FilesHandler.cs
+++ SupportTroubleshootingTool/Core/Handlers/FilesHandler.cs
@@ -12,21 +12,7 @@ namespace SupportTroubleshootingTool.Core.Contract
         public FilesHandler(SessionInfo currentSession)
         {
             _currentSession = currentSession;
-            List<TraceInfo> traces = _currentSession.SelectedTraces;
-            List<FileLogInfo> fileLogInfos = _currentSession.SelectedFileLogs;
-            // Create folder for each trace/filelog by description 
-            // create list of files paths to be collected for each filelog/trace
-            // copy files to folder
-            foreach(TraceInfo trace in traces) {
-                string outputfolder= $@"{_currentSession.SessionOtputFolderPath}/{"FileLogs"}/{trace.Description}";
-                if (!Directory.Exists(outputfolder)) { 
-                    Directory.CreateDirectory(outputfolder); 
-                }
-                CollectData(outputfolder,trace.TracesPath,trace.TraceFileName); 
-            }
-            foreach(FileLogInfo fileLogInfo in fileLogInfos) {
-                CollectData(fileLogInfo.Description, fileLogInfo.LogsPath, fileLogInfo.LogFileName);
-            }
+           
         }
 
         internal void CollectData(string outputfolder, string folderPath, string filter)
@@ -34,6 +20,11 @@ namespace SupportTroubleshootingTool.Core.Contract
 
             try
             {
+                if (!Directory.Exists(folderPath))
+                {
+                    new Utilities.Logger().WriteError("Logs Source Folders Not Found");
+                    throw new Exception("Logs Source Folders Not Found");
+                }
                 DateTime from = _currentSession.From;
                 DateTime to = _currentSession.To;
                 string[] filePaths = Directory.GetFiles(folderPath, filter);
@@ -53,6 +44,42 @@ namespace SupportTroubleshootingTool.Core.Contract
 
             }
 
+        }
+
+        internal void CollectData()
+        {
+            try
+            {
+                List<TraceInfo> traces = _currentSession.SelectedTraces;
+                List<FileLogInfo> fileLogInfos = _currentSession.SelectedFileLogs;
+                string from = _currentSession.From.ToString("yyyy-MM-dd-hh-mm");
+                string to = _currentSession.To.ToString("yyyy-MM-dd-hh-mm");
+                // Create folder for each trace/filelog by description 
+                // create list of files paths to be collected for each filelog/trace
+                // copy files to folder
+                foreach (TraceInfo trace in traces)
+                {
+                    string outputfolder = $@"{_currentSession.SessionOtputFolderPath}\{"OutputData"}\{from}_{to}\{"FileLogs"}\{trace.Description}";
+                    if (!Directory.Exists(outputfolder))
+                    {
+                        Directory.CreateDirectory(outputfolder);
+                    }
+                    CollectData(outputfolder, trace.TracesPath, trace.TraceFileName);
+                }
+                foreach (FileLogInfo fileLogInfo in fileLogInfos)
+                {
+                    string outputfolder = $@"{_currentSession.SessionOtputFolderPath}\{"OutputData"}\{from}_{to}\{"FileLogs"}\{fileLogInfo.Description}";
+                    if (!Directory.Exists(outputfolder))
+                    {
+                        Directory.CreateDirectory(outputfolder);
+                    }
+                    CollectData(outputfolder, fileLogInfo.LogsPath, fileLogInfo.LogFileName);
+                }
+            }
+            catch(Exception e) { 
+                new Utilities.Logger().WriteError(e);
+                throw;
+            }
         }
     }
 }
