fde95e03fe075b655e4daed327067840fa81a8a9
jacqueline2811 <55648045+jacqueline2811@users.noreply.github.com>
2020-02-27 10:24:33 +0200

    removed unnecassery dependies

--- SupportTroubleshootingTool/Core/Contract/SessionProvider.cs
+++ SupportTroubleshootingTool/Core/Contract/SessionProvider.cs
@@ -8,7 +8,7 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+
 
 namespace SupportTroubleshootingTool.Core.Contract
 {
