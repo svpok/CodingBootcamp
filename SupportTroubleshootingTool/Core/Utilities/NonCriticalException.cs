8ef41667eef714673456c61a17cd0f9d363c6f39
aakef <aakef1987@gmail.com>
2020-03-04 13:28:01 +0200

    add NonCriticalException class

---
+++ SupportTroubleshootingTool/Core/Utilities/NonCriticalException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SupportTroubleshootingTool.Core.Utilities
+{
+    [Serializable]
+    internal class NonCriticalException : Exception
+    {
+        public NonCriticalException()
+        {
+        }
+
+        public NonCriticalException(string message) : base(message)
+        {
+        }
+
+        public NonCriticalException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NonCriticalException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
